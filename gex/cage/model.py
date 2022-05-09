6314c5e483af4c9e62cfa847dcc6739683585003
张振豪 <zhangzhenhao@gous-MacBook-Pro-2.local>
2022-05-08 22:46:41 -0400

    epcot

--- gex/cage/model.py
+++ gex/cage/model.py
@@ -110,8 +110,9 @@ def build_pre_train_model(args):
             transfomer=transformer,
             num_class=args.num_class,
         )
-    model_path=os.path.abspath('EPCOT/pretraining/pretrain_dnase.pt')
-    pretrain_model.load_state_dict(torch.load(model_path, map_location='cpu'))
+    if args.load_pretrain:
+        model_path=os.path.abspath('EPCOT/pretraining/pretrain_dnase.pt')
+        pretrain_model.load_state_dict(torch.load(model_path, map_location='cpu'))
     if not args.fine_tune:
         for param in pretrain_model.parameters():
             param.requires_grad = False
